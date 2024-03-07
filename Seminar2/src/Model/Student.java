fa7bbb7c96c32bcfc403b7ebb294c9d675972b8d
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-03-07 14:30:12 +0200

    Update Student.java

--- Seminar2/src/Model/Student.java
+++ Seminar2/src/Model/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	private long sID;
 	private String name;
 	private String surname;
+	private String personal_number;
 	private static long counter = 10000;
 	
 	//2. get and set
@@ -16,13 +17,22 @@ public class Student {
 	}
 	public String getSurame(){
 		return surname;
-	}
-	
+	}	
 	public void setsID() {
 		sID = counter;
 		counter++;
+	}	
+	public String getPersonal_number() {
+		return personal_number;
 	}
-
+	
+	public void setPersonal_number(String personal_number) {
+		if(personal_number != null && personal_number.matches("[0-9]{6}-[0-9]{5}")) {
+			this.personal_number = personal_number;
+		}
+		else
+			this.personal_number = "Incorrect";
+	}
 	public void setName(String name) {
 		if(name != null && name.matches("[A-ZĒŪĪĻĶĢĀŽŠŅ]{1}[a-zēūīļķāžšņ]+")) {
 			this.name = name;
@@ -43,17 +53,20 @@ public class Student {
 		setsID();
 		setName("Eksistētājs");
 		setSurname("Mūžīgais");
+		setPersonal_number("030487-12430");
 	}	
 	
-	public Student(String name, String surname) {
+	public Student(String name, String surname, String personal_number) {
 		setsID();
 		setName(name);
 		setSurname(surname);
+		setPersonal_number(personal_number);
 	}
 	
 	//4. toString
 	public String toString() {
-		return sID + ": " + name + " " + surname;
+		return sID + ": " + name + " " + surname + " " + personal_number;
 	}	
+
 	
 }
