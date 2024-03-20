3067b158137091753bbd55f30fe8f3039aaf71ff
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-03-20 15:52:38 +0200

    Create AbstractCustomer.java

---
+++ EOmniva_2024_Dzelme/src/Model/AbstractCustomer.java
@@ -0,0 +1,13 @@
+/* AbstractCustomer (9 punkti) - abstrakta klase, no kuras neveidos objektus, bet to izmantot kā bāzes klasi citām klasēm. Satur
+idC, address, phoneNo, customerCode, parcels mainīgos;
+i. Nodrošināt idC unikāla uzstādīšanu, izmantojot “autoincrement” algoritmu (1 punkts);
+ii. setCustomerCode funkciju izveidot abstraktu, jo tā būs jāpārraksta CustomerAsPerson un CustomerAsCompany klasēs
+(1 punkts);
+iii. Izveidot addNewParcel funkciju, kas ļauj pievienot paciņu pircējam. Šeit noteikti jāpārbauda, vai tāda paciņa jau
+neeksistē pircēja paciņu sarakstā (1 punkts); */
+
+package Model;
+
+public abstract class AbstractCustomer {
+
+}
