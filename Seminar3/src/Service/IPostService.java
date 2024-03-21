9f60635acb6428dd73e4f7232f6ad18f9efc9ffe
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-03-21 14:24:27 +0200

    Update IPostService.java

--- Seminar3/src/Service/IPostService.java
+++ Seminar3/src/Service/IPostService.java
@@ -1,9 +1,10 @@
 package Service;
 
+import Model.Post;
 import Model.PostType;
 
 public interface IPostService {
 
-	public abstract void publishPost(String title, String msg, PostType type) throws Exception;
+	public abstract Post publishPost(String title, String msg, PostType type) throws Exception;
 	
 }
