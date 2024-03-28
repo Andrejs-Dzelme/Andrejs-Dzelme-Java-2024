e616b1ffdc304aa04b50971e16a825a78eef9d74
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-03-28 21:34:07 +0200

    Update CustomerAsPerson.java

--- EOmniva_2024_Dzelme/src/Model/CustomerAsPerson.java
+++ EOmniva_2024_Dzelme/src/Model/CustomerAsPerson.java
@@ -1,35 +1,12 @@
 package Model;
 
-public class CustomerAsPerson extends Person{
+public class CustomerAsPerson extends AbstractCustomerAsPerson{
 	//1. Variables
-	private Address address;
-	private String phone;
 	private String  customerCode;
 	private AbstractCustomer cID;
+	//private String holder = person.getPersonal_number();
 
 	//2. gets and sets
-	public Address getAddress() {
-		return address;
-	}
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setAddress(Address address) {
-		if(address != null) {
-			this.address = address;
-		}
-		else
-			this.address = new Address();
-	}
-	public void setPhone(String phone) {
-		if(phone != null && phone.matches("[+0-9]{3}[ 0-9]+")) {
-			this.phone = phone;
-		}
-		else
-			this.phone = "+371 00000000";
-		this.phone = phone;
-	}
 	public String getCustomerCode() {
 		return customerCode;
 	}
@@ -37,23 +14,19 @@ public class CustomerAsPerson extends Person{
 	//3. Constructors
 	public CustomerAsPerson() {
 		super();
-		setAddress(new Address());
-		setPhone(phone.toString());
 	}	
-	public CustomerAsPerson(String name, String surname, String personal_number, Address address, String phone) {
-		super(name, surname, personal_number);
-		setAddress(address);
-		setPhone(phone);
+	public CustomerAsPerson(String name, String surname, String personal_number, Address address, String phoneNo) {
+		super(name, surname, personal_number, address, phoneNo);
 	}
 	
 	//4. toString
 	public String toString() {
-		return super.getName()+" "+super.getSurname()+", P/K: "+super.getPersonal_number()+"\nAddress nr."+address+"; Phone: "+phone;
+		return person.getName()+" "+person.getSurname()+", P/K: "+person.getPersonal_number()+"\nAddress: "+super.getAddress()+"; Phone: "+super.getPhoneNo()+"\n";
 	}
 	
 	//5. other functions
 	public void setCustomerCode() {
-		this.customerCode = cID+"_Person_"+super.getPersonal_number();
+		this.customerCode = cID+"_Person_"+super.getPersonal_number();//holder.toString(); Not working correctly
 	}
 
 }
